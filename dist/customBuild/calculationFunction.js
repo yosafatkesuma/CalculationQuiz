1824b45827a4fb74eefe051cee78debccf7b5889
yosafatkesuma <da005kage@gmail.com>
2019-12-07 19:01:22 +0700

    Add(calculationFunction.js) : timer

--- dist/customBuild/calculationFunction.js
+++ dist/customBuild/calculationFunction.js
@@ -1,6 +1,9 @@
+function getId(id){
+    return document.getElementById(id)
+}
 
 function intervalTime(){
-    counter.end = 10;
+    counter.end = 120;
     counter.min = document.getElementById("cd-min");
     counter.sec = document.getElementById("cd-sec");
     
@@ -23,12 +26,20 @@ function intervalTime(){
 }
 function timer(){
     intervalTime()
-    return setTimeout(function(){ console.log("Time out")}, 10000)
+
+    if (calculatorBox.style.visibility === 'hidden') {
+        calculatorBox.style.visibility = 'visible'
+        startButton.style.visibility = 'hidden'
+        timers.style.visibility = 'visible'
+    } else {
+        calculatorBox.style.visibility = 'hidden'
+    }
+    timesOut = setTimeout(function() { 
+        alert("Timeout\nwaktu anda telah habis")
+        window.location.reload() }, 120000)
+    return timesOut
 }
 
-function getId(id){
-    return document.getElementById(id)
-}
 
 function layout(levels, moves, goals, results, num1, num2, num3, num4, num5, num6){
     level.innerHTML = "Level : " + levels
@@ -65,21 +76,36 @@ function chance (buts) {
     console.log(tempMove)
     if (tempMove > 0){
         if(tempResult == tempGoal){
+            if(tempMove != dataList[i][1])
+            {
+                alert("Mantap Sekali anda bisa menyelesaikan tantangan yang ada sebelum moves menyentuh 0")
+            }
+
             i++
+
             if (i > dataList.length-1) {
                 alert("Fabulous, anda telah menyelesaikan semua tantangan\nAnda dapat mengulanginya dengan cara menekan F5 saja")
+                clearTimeout(timesOut)
+                counter.end = 0
+                clearInterval(counter.ticker)
             } else {
                 layout(dataList[i][0], dataList[i][1], dataList[i][2], dataList[i][3], dataList[i][4], dataList[i][5], dataList[i][6], dataList[i][7], dataList[i][8], dataList[i][9])
             }
+            
         }
     } else if (tempMove == 0) {
         if(tempResult == tempGoal){
             i++
+
             if (i > dataList.length-1) {
                 alert("Fabulous, anda telah menyelesaikan semua tantangan\nAnda dapat mengulanginya dengan cara menekan F5 saja")
+                clearTimeout(timesOut)
+                counter.end = 0
+                clearInterval(counter.ticker)
             } else {
                 layout(dataList[i][0], dataList[i][1], dataList[i][2], dataList[i][3], dataList[i][4], dataList[i][5], dataList[i][6], dataList[i][7], dataList[i][8], dataList[i][9])
             }
+
         } else {
             let ans = confirm("Do you want to restart the calculation?")
             if(ans == true){
@@ -91,8 +117,12 @@ function chance (buts) {
         }
         
     }
+    
 }
-
+var timesOut = 0
+var timers = getId("timers")
+var startButton = getId("start-button")
+var calculatorBox =  getId("calculator-quiz")
 var counter = {};
 var tempMove = 0
 var tempResult = 0
@@ -110,6 +140,10 @@ var but6 = getId("button6")
 
  //level, move, goal, tempResult, num1, num2, num3, num4, num5, num6
 
-var dataList = [[1, 3, 12, 5, "+3", "-4", "+4", "+5", "+6", "-6"], [2, 2, 4, 5, "+2", "+4", "*5", "+5", "-6", "-9"], [3, 2, 4, 5, "/2", "+9", "*7", "-5", "-8", "-9"]]
+var dataList = [[1, 3, 12, 5, "+3", "-4", "+4", "+5", "+6", "-6"], [2, 2, 4, 5, "+2", "+4", "*5", "+5", "-6", "-9"], 
+                [3, 4, 22, 5, "/2", "+4", "*5", "+5", "-6", "-3"], [4, 2, 4, 5, "/2", "+9", "*7", "+3", "-8", "-9"],
+                [5, 2, 5, 4, "/2", "+9", "*7", "-5", "-8", "-9"]]
 var i = 0
 layout(dataList[i][0], dataList[i][1], dataList[i][2], dataList[i][3], dataList[i][4], dataList[i][5], dataList[i][6], dataList[i][7], dataList[i][8], dataList[i][9])
+calculatorBox.style.visibility = 'hidden'
+timers.style.visibility = 'hidden'
